77b11e947e5458dd43d0b26b3e0f13e80caf3ec6
Handensaken <60339724+Handensaken@users.noreply.github.com>
2020-03-16 08:38:46 +0100

    Added base search box, Got value from said search box

---
+++ js/script.js
@@ -0,0 +1,5 @@
+let submitButton = document.querySelector("#test");
+submitButton.addEventListener("click", ()=>{
+    let SearchBar = document.querySelector("#SearchBar").value;
+    console.log(SearchBar);
+})
