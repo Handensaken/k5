3d1b333521ce2fa2e2956d421c4722c27c99edb9
Handensaken <60339724+Handensaken@users.noreply.github.com>
2020-03-19 13:12:00 +0100

    skapat en temporär array för att testa array.includes. Lagt till så att man kan söka efter ett nyckelord eller en nyckelbokstav och så matar konsollen ut det man kanske kan leta efter.; Fixat lite information om sidan samt lorem.; Strukturerat sidan lite med grids, margin och padding

--- js/script.js
+++ js/script.js
@@ -1,5 +1,11 @@
 let submitButton = document.querySelector("#test");
+let test = ["hej","tjena","uwu"];
 submitButton.addEventListener("click", ()=>{
     let SearchBar = document.querySelector("#SearchBar").value;
     console.log(SearchBar);
+    for (let i = 0; i < test.length; i++) {
+        if(test[i].includes(SearchBar)){
+            console.log(test[i]);
+        }
+    }
 })
