71360cfcdb1c1578510130880bba63e92d016e7f
Handensaken <60339724+Handensaken@users.noreply.github.com>
2020-03-20 11:08:37 +0100

    Structured page with CSS; added a button to add a song; Made new HTML elements when button is pressed. Tried to add text content to 2d array (WIP)

--- js/script.js
+++ js/script.js
@@ -1,5 +1,13 @@
 let submitButton = document.querySelector("#test");
+let newSong = document.querySelector("#new");
 let test = ["hej","tjena","uwu"];
+let testNumberOfSongs = 0;
+
+let newTest = [
+
+];
+
+
 submitButton.addEventListener("click", ()=>{
     let SearchBar = document.querySelector("#SearchBar").value;
     console.log(SearchBar);
@@ -9,3 +17,30 @@ submitButton.addEventListener("click", ()=>{
         }
     }
 })
+
+newSong.addEventListener("click", ()=>{
+    Hello();
+    AddLyrics();
+
+    let newTitle = document.querySelector(".myNewDiv").textContent;
+    console.log(newTitle);
+
+})
+let newDiv;
+function Hello(){
+    let stringTest = "hello";
+    test.push(stringTest);
+    newDiv = document.createElement("div");
+    newDiv.setAttribute("class", "myNewDiv");
+    let text = document.createTextNode("test hello");
+    newDiv.appendChild(text);
+    document.body.appendChild(newDiv);
+}  
+function AddLyrics(){
+    let newStringTest = "lorem ipsum dolor sit"
+    let newLyricDiv = document.createElement("div");
+    newLyricDiv.setAttribute("class", "lyrics");
+    let text = document.createTextNode(newStringTest);
+    newLyricDiv.appendChild(text);
+    newDiv.appendChild(newLyricDiv);
+}
