4a1ee2e81ad33e306956f594c1ef150f7d3d814a
Handensaken <60339724+Handensaken@users.noreply.github.com>
2020-03-27 10:44:26 +0100

    edited CSS; Fixed Search bar

--- js/script.js
+++ js/script.js
@@ -1,22 +1,40 @@
 let submitButton = document.querySelector("#test");
 let newSong = document.querySelector("#new");
-let test = ["hej","tjena","uwu"];
-let testNumberOfSongs = 0;
+
 
 let newTest = [
-
+    
 ];
+let songs = document.querySelectorAll(".songs");
+let lyrics = document.querySelectorAll(".lyrics");
+let titles = document.querySelectorAll(".titles");
+for (let index = 0; index < songs.length; index++) {
+    const title = titles[index].textContent;   
+    const lyric = lyrics[index].textContent;
+    
+    newTest.push([title,lyric]);
+}
 
 
 submitButton.addEventListener("click", ()=>{
     let SearchBar = document.querySelector("#SearchBar").value;
-    console.log(SearchBar);
-    for (let i = 0; i < test.length; i++) {
-        if(test[i].includes(SearchBar)){
-            console.log(test[i]);
-        }
+    console.log(SearchBar); 
+    for (let i = 0; i < newTest.length; i++) {
+            if(newTest[i].includes(SearchBar)){
+                songs[i].style.display = "block";
+            }
+            else if(newTest[i][1].includes(SearchBar)){
+                songs[i].style.display = "block";
+            }
+            else{
+                songs[i].style.display = "none";
+            }
+            if(SearchBar == ""){
+                songs[i].style.display = "none";
+            }
     }
 })
+
 
 newSong.addEventListener("click", ()=>{
     Hello();
