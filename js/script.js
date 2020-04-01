9c5b21ef7f50e846337f1abf3db9b905de8a0fbb
Handensaken <60339724+Handensaken@users.noreply.github.com>
2020-04-01 12:07:44 +0200

    fixed id:s; added one song; removed user added songs function - database needed; search made case insensitive

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-let submitButton = document.querySelector("#test");
+let submitButton = document.querySelector("#goButton");
 let newSong = document.querySelector("#new");
 
 
@@ -9,18 +9,18 @@ let songs = document.querySelectorAll(".songs");
 let lyrics = document.querySelectorAll(".lyrics");
 let titles = document.querySelectorAll(".titles");
 for (let index = 0; index < songs.length; index++) {
-    const title = titles[index].textContent;   
-    const lyric = lyrics[index].textContent;
-    
+    const title = titles[index].textContent.toUpperCase();   
+    const lyric = lyrics[index].textContent.toUpperCase();
+    console.log(title);
     newTest.push([title,lyric]);
 }
 
 
 submitButton.addEventListener("click", ()=>{
-    let SearchBar = document.querySelector("#SearchBar").value;
-    console.log(SearchBar); 
+    let SearchBar = document.querySelector("#SearchBar").value.toUpperCase();
+    console.log(SearchBar);
     for (let i = 0; i < newTest.length; i++) {
-            if(newTest[i].includes(SearchBar)){
+            if(newTest[i][0].includes(SearchBar)){
                 songs[i].style.display = "block";
             }
             else if(newTest[i][1].includes(SearchBar)){
@@ -34,31 +34,3 @@ submitButton.addEventListener("click", ()=>{
             }
     }
 })
-
-
-newSong.addEventListener("click", ()=>{
-    Hello();
-    AddLyrics();
-
-    let newTitle = document.querySelector(".myNewDiv").textContent;
-    console.log(newTitle);
-
-})
-let newDiv;
-function Hello(){
-    let stringTest = "hello";
-    test.push(stringTest);
-    newDiv = document.createElement("div");
-    newDiv.setAttribute("class", "myNewDiv");
-    let text = document.createTextNode("test hello");
-    newDiv.appendChild(text);
-    document.body.appendChild(newDiv);
-}  
-function AddLyrics(){
-    let newStringTest = "lorem ipsum dolor sit"
-    let newLyricDiv = document.createElement("div");
-    newLyricDiv.setAttribute("class", "lyrics");
-    let text = document.createTextNode(newStringTest);
-    newLyricDiv.appendChild(text);
-    newDiv.appendChild(newLyricDiv);
-}
